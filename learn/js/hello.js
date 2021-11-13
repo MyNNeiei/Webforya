240bb5a3b4df3e21f50bed86155a444819f304f8
NNMIMI <kullananzaza@gmail.com>
2021-11-14 00:27:07 +0700

    wowza

---
+++ learn/js/hello.js
@@ -0,0 +1,12 @@
+console.log('hello kullanan');
+console.log('10');
+
+//
+
+console.log(10 + 10);
+console.log(10 % 3);
+console.log((12+10)*4 / 5);
+console.log('This is '+'NN' + 'say hi');
+console.log('10' + 10);
+
+////
